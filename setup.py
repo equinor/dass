ce710b6dd6f053460ca0b659688f5b4ec1345d6b
Feda Curic <feda.curic@gmail.com>
2026-03-18 09:07:58 +0100

    Remove jupytext

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ setup(
         "p_tqdm>=1.3.3,<2.0.0",
         "matplotlib>=3.5.1,<4.0.0",
         "jupyter",
-        "jupytext",
         "numpy",
         "pandas>=2.0.0",
         "scipy>=1.9.0",
